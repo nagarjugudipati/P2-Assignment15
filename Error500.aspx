fbed7b5c8451b501b7e94d0b486d45dd64ba9729
nagarajugudipati1@gmail.com <nagarajugudipati1@gmail.com>
2023-08-31 22:29:53 +0530

    P2-Assignment15

---
+++ Error500.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Error500.aspx.cs" Inherits="WebAppAssignment15.Error500" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+    <body>
+    <form id="form1" runat="server">
+        <div>
+            <div>
+                <h2 style="text-align;center; background-color:red; color:whitesmoke">Internal Server Error!!!</h2>
+                    </div>
+        </div>
+    </form>
+</body>
+</html>
+
---
+++ Global.asax.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace WebAppAssignment15
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+        void Application_Error(object sender, EventArgs e)
+        {
+            Exception ex = Server.GetLastError();
+            Server.ClearError();
+
+            //Log the exception if needed
+            //Redirect to the appropriate erroe page based on the exception
+            if (ex is HttpException)
+            {
+                HttpException httpEx = ex as HttpException;
+                int statusCode = httpEx.GetHttpCode();
+                if (statusCode == 404)
+                {
+                    Response.Redirect("~/Error404.aspx");
+                }
+                else
+                {
+                    Response.Redirect("~/Error500.aspx");
+
+                }
+            }
+            else
+            {
+                Response.Redirect("~/Error.aspx");
+            }
+        }
+    }
+}
